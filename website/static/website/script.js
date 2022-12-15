3a640ac4ffdbe675d09749d4221994fe6d8efac6
iamkileR <jakubsadza@gmail.com>
2022-12-15 14:51:04 +0100

    some updates

--- website/static/website/script.js
+++ website/static/website/script.js
@@ -21,13 +21,14 @@
 		}
 
 	// calc score with answerScore function
-		var calcScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') + answerScore('q4'));
+		var calcScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') + answerScore('q4')
+			+ answerScore('q5') + answerScore('q6') + answerScore('q7')+ answerScore('q8') + answerScore('q9') + answerScore('q10'));
 		console.log("CalcScore: " + calcScore); // it works!
 
 	// function to return correct answer string
 		function correctAnswer (correctStringNo, qNumber) {
 			console.log("qNumber: " + qNumber);  // logs 1,2,3,4 after called below
-			return ("The correct answer for question #" + qNumber + ": &nbsp;<strong>" +
+			return ("Poprawna odpowiedz na pytanie #" + qNumber + ": &nbsp;<strong>" +
 				(document.getElementById(correctStringNo).innerHTML) + "</strong>");
 			}
 
@@ -44,6 +45,24 @@
 		if (answerScore('q4') === 0) {
 			document.getElementById('correctAnswer4').innerHTML = correctAnswer('correctString4', 4);
 		}
+		if (answerScore('q5') === 0) {
+			document.getElementById('correctAnswer5').innerHTML = correctAnswer('correctString5', 5);
+		}
+		if (answerScore('q6') === 0) {
+			document.getElementById('correctAnswer6').innerHTML = correctAnswer('correctString6', 6);
+		}
+		if (answerScore('q7') === 0) {
+			document.getElementById('correctAnswer7').innerHTML = correctAnswer('correctString7', 7);
+		}
+		if (answerScore('q8') === 0) {
+			document.getElementById('correctAnswer8').innerHTML = correctAnswer('correctString8', 8);
+		}
+		if (answerScore('q9') === 0) {
+			document.getElementById('correctAnswer9').innerHTML = correctAnswer('correctString9', 9);
+		}
+		if (answerScore('q10') === 0) {
+			document.getElementById('correctAnswer10').innerHTML = correctAnswer('correctString10', 10);
+		}
 
 	// calculate "possible score" integer
 		var questionCountArray = document.getElementsByClassName('question');
@@ -54,7 +73,7 @@
 		}
 
 	// show score as "score/possible score"
-		var showScore = "Twój wynik: " + calcScore +"/" + questionCounter;
+		var showScore = "Twój wynik: " + calcScore + "/" + questionCounter;
 	// if 4/4, "perfect score!"
 		if (calcScore === questionCounter) {
 			showScore = showScore + "&nbsp; <strong>Świetny wynik!</strong>"
