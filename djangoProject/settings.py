0c5b46f34197188de242eb729c3afee33d5a03ac
iamkileR <jakubsadza@gmail.com>
2023-01-23 16:49:44 +0100

    updated settings and req

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -117,3 +117,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
